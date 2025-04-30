2f5aab702f306555722f16cf15d5c6c68171f262
azubangithub <azuban2008@gmail.com>
2025-04-30 19:44:38 +0300

    v2

---
+++ install.ps1
@@ -0,0 +1,9 @@
+# Встановлення залежностей для сервера
+npm install
+
+# Встановлення залежностей для клієнта
+cd client
+npm install
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+cd .. 
